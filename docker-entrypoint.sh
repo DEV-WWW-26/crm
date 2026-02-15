21ab01f7bfc19631de5141de854c308a9e1f342d
Developer.KZ <developer.kz@gmail.com>
2026-02-16 03:16:52 +0500

    making a progress

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Use envsubst to replace env vars in init.sql and execute via psql (for Postgres)
-# For MySQL you would use 'mysql' client instead of 'psql'
-envsubst < /docker-entrypoint-initdb.d/init.sql | psql -v ON_ERROR_STOP=1 --username "$DB_USER" --dbname "$DB_NAME" --pass "$DB_PASS"
-
-# Call the original entrypoint script to start the database server
-exec docker-entrypoint.sh "$@"
