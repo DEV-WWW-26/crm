8691cd9d1817cd5e8a97deb4888b2cd2d6a55d55
Developer.KZ <developer.kz@gmail.com>
2026-02-16 19:49:41 +0500

    making a progress

--- public/app/services/DbService.php
+++ public/app/services/DbService.php
@@ -22,12 +22,6 @@ class DbService
     public function connect()
     {
         try {
-            // $user = $this->config->getUser();
-            $user = $this->config->getUser() . '@%';
-            echo '<p>User: ' . $user . '</p>';
-            echo '<p>Server: ' . $this->config->getServer() . '</p>';
-            echo '<p>Db: ' . $this->config->getDb() . '</p>';
-            echo '<p>pass: ' . $this->config->getPass() . '</p>';
             $this->connection = new mysqli($this->config->getServer(), $this->config->getUser(), $this->config->getPass(), $this->config->getDb());
         } catch (\Exception $e) {
             exit("An error occurred on creating DB connection: " . $e->getMessage());
--- public/app/views/registration/post_registration.php
+++ public/app/views/registration/post_registration.php
@@ -10,3 +10,4 @@ use App\Service\AuthService;
 $authService = new AuthService();
 $user = new User($_POST["firstname"], $_POST["lastname"], $_POST["email"], $_POST["password"]);
 $authService->registerUser($user);
+
