6021c34f40a1c080ab233283a9e3f7107207c800
Developer.KZ <developer.kz@gmail.com>
2026-02-18 02:37:14 +0500

    making a progress

---
+++ public/app/views/meeting/meeting_report.php
@@ -0,0 +1,14 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/app/views/header.html';
+?>
+
+<h2>Отчет по встречам</h2>
+
+<?php
+include 'meeting_report.html';
+?>
+
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/app/views/footer.html';
+?>
+
