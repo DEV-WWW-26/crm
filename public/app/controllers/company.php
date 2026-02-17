b88594be60557a3e022aac03ddc592e1e8e186cb
Developer.KZ <developer.kz@gmail.com>
2026-02-17 12:23:19 +0500

    making a progress

--- public/app/controllers/company.php
+++ public/app/controllers/company.php
@@ -2,15 +2,22 @@
 include_once $_SERVER['DOCUMENT_ROOT'] . "/app/services/DbService.php";
 include_once $_SERVER['DOCUMENT_ROOT'] . "/app/services/CompanyService.php";
 include_once $_SERVER['DOCUMENT_ROOT'] . "/app/models/Company.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/app/models/Address.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/app/models/City.php";
 
 include $_SERVER['DOCUMENT_ROOT'] . '/app/views/header.html';
 
+use app\models\Address;
+use app\models\Category;
+use app\models\City;
 use app\models\Company;
 use app\services\CompanyService;
 
 $companyService = new CompanyService();
-$user = new User($_POST["firstname"], $_POST["lastname"], $_POST["email"], $_POST["password"]);
-$company = new Company();
+$city = new City($_POST["city"]);
+$address = new Address($city, $_POST["street"], $_POST["building"]);
+$category = new Category($_POST["category"]);
+$company = new Company($address, $category, $_POST["title"], $_POST["email"], $_POST["phone"]);
 
 $companyService->addCompany($company);
 
