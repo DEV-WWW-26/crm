f99e14380c391ce71a6950de39a385cda95c0ea0
Developer.KZ <developer.kz@gmail.com>
2026-02-20 16:58:51 +0500

    making a progress

--- public/app/controllers/registration.php
+++ public/app/controllers/registration.php
@@ -1,15 +1,20 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . "/app/services/AuthService.php";
 include_once $_SERVER['DOCUMENT_ROOT'] . "/app/services/DbService.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/app/services/NavigatorService.php";
 include_once $_SERVER['DOCUMENT_ROOT'] . "/app/models/User.php";
 
 include $_SERVER['DOCUMENT_ROOT'] . '/app/views/header.html';
 
 use App\Model\User;
 use app\services\AuthService;
+use app\services\NavigatorService;
 
 $authService = new AuthService();
 $user = new User($_POST["firstname"], $_POST["lastname"], $_POST["email"], $_POST["password"]);
 $authService->registerUser($user);
+
+$navigatorService = new NavigatorService();
+$navigatorService->goHome();
 
 include $_SERVER['DOCUMENT_ROOT'] . '/app/views/footer.html';
